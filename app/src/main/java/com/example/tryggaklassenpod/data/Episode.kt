2c16d82ff845e051ba041e12c24320776316f856
Liis U <liis.usin@gmail.com>
2023-09-30 16:27:17 +0200

    add data class for episode

--- app/src/main/java/com/example/tryggaklassenpod/data/Episode.kt
+++ app/src/main/java/com/example/tryggaklassenpod/data/Episode.kt
@@ -1,3 +1,15 @@
 package com.example.tryggaklassenpod.data
 
-data class Episode()
+import java.time.Duration
+
+data class Episode(
+    val imageUrl: String,
+    val audio: String,
+    val duration: Duration,
+
+    val title: String,
+    val description: String,
+
+    val comments: List<Comments>,
+    val categories: List<String>
+)
