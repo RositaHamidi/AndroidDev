e9f2ac996c3da96b0c10b910404fcd0b1fe011ea
Liis U <liis.usin@gmail.com>
2023-10-03 20:03:43 +0200

    add Date() function to createdAt

--- app/src/main/java/com/example/tryggaklassenpod/data/Comments.kt
+++ app/src/main/java/com/example/tryggaklassenpod/data/Comments.kt
@@ -2,12 +2,11 @@ package com.example.tryggaklassenpod.data
 
 import java.util.Date
 
-data class Comments(
+data class Comment(
     val comment: String,
     val author: String,
 
-    val createdAt: Date,
+    val createdAt: Date = Date(),
     val approved: Boolean = false,
-
     val likes: Int = 0
 )
