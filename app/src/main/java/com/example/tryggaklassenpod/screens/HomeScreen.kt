732605f138bfea8f8d79f9aa86115e78ded137e0
RositaHamidi <130679452+RositaHamidi@users.noreply.github.com>
2023-10-03 21:47:49 +0200

    home Screen basics

--- app/src/main/java/com/example/tryggaklassenpod/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tryggaklassenpod/screens/HomeScreen.kt
@@ -16,92 +16,293 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tryggaklassenpod.R
+import com.example.tryggaklassenpod.ui.theme.md_theme_dark_primaryContainer
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
-    Column {
+    Column (
+        modifier = Modifier.background(Color.Transparent)
+    ){
         TopAppBar(
-            modifier = Modifier.background(Color.Transparent),
+
             title = { Text(
                 text = "Trygga Klassen",
                 textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth()
             )},
             navigationIcon = {
-                // Place your navigation icon (menu icon) here
-                IconButton(onClick = { /* Handle menu icon click */ }) {
-                    Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+                IconButton(
+                    onClick = { /* Handle menu icon click */ },
+                    ) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = null,
+                        )
                 }
             },
-        )
+
+            )
 
         Column(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
-                .padding(5.dp),
+                .padding(5.dp)
+                ,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.imageplaceholder),
-                contentDescription = "sample",
-                contentScale = ContentScale.FillBounds
-            )
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = "Podcast 1")
-            Spacer(modifier = Modifier.padding(15.dp))
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.imageplaceholder),
+                    contentDescription = "sample",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.size(40.dp)
 
-            Image(
-                painter = painterResource(id = R.drawable.imageplaceholder),
-                contentDescription = "sample"
-            )
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = "Podcast 2")
-            Spacer(modifier = Modifier.padding(15.dp))
+                    )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Column (verticalArrangement = Arrangement.Center){
+                    Text(text = "Podcast 1",
+                        color = Color.White,
+                        textAlign = TextAlign.Start,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Details / description of each podcast",
+                        color = Color.White,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                }
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
 
-            Image(
-                painter = painterResource(id = R.drawable.imageplaceholder),
-                contentDescription = "sample"
-            )
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = "Podcast 3")
-            Spacer(modifier = Modifier.padding(15.dp))
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.imageplaceholder),
+                    contentDescription = "sample",
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Column (verticalArrangement = Arrangement.Center){
+                    Text(text = "Podcast 2",
+                        color = Color.White,
+                        textAlign = TextAlign.Start,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Details / description of each podcast",
+                        color = Color.White,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                }
+            }
 
-            Image(
-                painter = painterResource(id = R.drawable.imageplaceholder),
-                contentDescription = "sample"
-            )
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = "Podcast 4")
-            Spacer(modifier = Modifier.padding(15.dp))
+            Spacer(modifier = Modifier.padding(10.dp))
 
-            Image(
-                painter = painterResource(id = R.drawable.imageplaceholder),
-                contentDescription = "sample"
-            )
-            Spacer(modifier = Modifier.padding(5.dp))
-            Text(text = "Podcast 5")
-            Spacer(modifier = Modifier.padding(15.dp))
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.imageplaceholder),
+                    contentDescription = "sample",
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Column (verticalArrangement = Arrangement.Center){
+                    Text(text = "Podcast 3",
+                        color = Color.White,
+                        textAlign = TextAlign.Start,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Details / description of each podcast",
+                        color = Color.White,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.imageplaceholder),
+                    contentDescription = "sample",
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Column (verticalArrangement = Arrangement.Center){
+                    Text(text = "Podcast 4",
+                        color = Color.White,
+                        textAlign = TextAlign.Start,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Details / description of each podcast",
+                        color = Color.White,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.imageplaceholder),
+                    contentDescription = "sample",
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Column (verticalArrangement = Arrangement.Center){
+                    Text(text = "Podcast 5",
+                        color = Color.White,
+                        textAlign = TextAlign.Start,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Details / description of each podcast",
+                        color = Color.White,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.imageplaceholder),
+                    contentDescription = "sample",
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Column (verticalArrangement = Arrangement.Center){
+                    Text(text = "Podcast 6",
+                        color = Color.White,
+                        textAlign = TextAlign.Start,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Details / description of each podcast",
+                        color = Color.White,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                }
+            }
+
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.imageplaceholder),
+                    contentDescription = "sample",
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Column (verticalArrangement = Arrangement.Center){
+                    Text(text = "Podcast 7",
+                        color = Color.White,
+                        textAlign = TextAlign.Start,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Details / description of each podcast",
+                        color = Color.White,
+                        textAlign = TextAlign.Justify,
+                        modifier = Modifier
+                            .wrapContentHeight()
+                            .fillMaxSize())
+                }
+            }
+
         }
 
-        BottomAppBar {
+        BottomAppBar (
+        ){
             /* for later */
         }
 
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun homeScreenPreview(){
+    HomeScreen()
 }
