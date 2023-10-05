1de8462fa0d4484041597f5345095d1da2356a9e
Fera 13 <fera6055@gmail.com>
2023-10-05 22:40:57 +0200

    Default fonts change unfinished

--- app/src/main/java/com/example/tryggaklassenpod/ui/theme/Type.kt
+++ app/src/main/java/com/example/tryggaklassenpod/ui/theme/Type.kt
@@ -5,15 +5,75 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.unit.sp
+import com.ad_coding.googlefontsapp.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Open Sans")
+val fontName2 = GoogleFont("Segoe UI")
+
+val fontFamilyH = FontFamily(
+    Font(
+        googleFont = fontName,
+        fontProvider = provider
+    )
+)
+
+val fontFamilyB = FontFamily(
+    Font(
+        googleFont = fontName2,
+        fontProvider = provider
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = fontFamilyH,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = fontFamilyH,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = fontFamilyH,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = fontFamilyH,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = fontFamilyH,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = fontFamilyH,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = fontFamilyB,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = fontFamilyB,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
