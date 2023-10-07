dc9431ea6ebf1fa548487b406d40581bede6dcfb
Anazeb <anam.aurangzeb@gmail.com>
2023-10-08 01:10:38 +0200

    Edited the UploadPodcast screen

--- app/src/main/java/com/example/tryggaklassenpod/screens/UploadPodcast.kt
+++ app/src/main/java/com/example/tryggaklassenpod/screens/UploadPodcast.kt
@@ -1,10 +1,14 @@
 package com.example.tryggaklassenpod.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -17,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun UploadPodcast(navController: NavController) {
         var podcastName by remember { mutableStateOf("") }
@@ -24,22 +29,46 @@ import androidx.navigation.NavController
 
         Column(
             modifier = Modifier.padding(16.dp)
-        ) {
-            BasicTextField(
+        )
+        {
+                Text(
+                    text = "Add the title/name of the podcast",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(16.dp)
+                )
+            OutlinedTextField(
                 value = podcastName,
-                onValueChange = { },
+                onValueChange = {newValue ->
+                    podcastName = newValue },
+                label = { Text("Title") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Add the summary/description of the podcast",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(16.dp)
             )
 
-            BasicTextField(
+            OutlinedTextField(
                 value = podcastDescription,
-                onValueChange = { },
+                onValueChange = {newValue ->
+                    podcastDescription = newValue },
+                label = { Text("Description ") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
 
             Button(
                 onClick = {
                 }
             ) {
-                Text(text = "Upload")
+                Text(text = "Upload podcast")
             }
         }
     }
