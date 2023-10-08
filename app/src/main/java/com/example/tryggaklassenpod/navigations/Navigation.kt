ad4ab497712d3f7f32e03cfc48013741f393430f
Liis U <liis.usin@gmail.com>
2023-10-08 18:38:31 +0200

    Player screen navigation and remove unused imports

--- app/src/main/java/com/example/tryggaklassenpod/navigations/Navigation.kt
+++ app/src/main/java/com/example/tryggaklassenpod/navigations/Navigation.kt
@@ -1,19 +1,22 @@
 package com.example.tryggaklassenpod.navigations
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import com.example.tryggaklassenpod.screens.Screen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.tryggaklassenpod.screens.AboutScreen
 import com.example.tryggaklassenpod.screens.HomeScreen
+import com.example.tryggaklassenpod.screens.PlayerScreen
 
 
 @Composable
-fun Navigation(){
+fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.HomeScreen.route ){
 
+    NavHost(navController = navController, startDestination = Screen.HomeScreen.route){
 
         composable(route = Screen.HomeScreen.route){
             HomeScreen(navController = navController)
@@ -23,7 +26,21 @@ fun Navigation(){
             AboutScreen(navController = navController)
         }
 
-
-
+        composable(
+            route = "${Screen.PlayerScreen.route}/{episodeId}",
+            arguments = listOf(
+                navArgument(name = "episodeId") {
+                    type = NavType.IntType
+                }
+            )
+        ) {index ->
+            val episodeId = index.arguments?.getInt("episodeId")
+            PlayerScreen(
+                episodeId = episodeId,
+                goBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
     }
 }
