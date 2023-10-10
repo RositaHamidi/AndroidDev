c5378e021938f8f3c925c6fa63b1291a3725e4bf
Liis U <liis.usin@gmail.com>
2023-10-10 17:14:01 +0200

    dataclasses updated regards to the database, also dummydata(if someone is still using..)

--- app/src/main/java/com/example/tryggaklassenpod/dataClasses/Episode.kt
+++ app/src/main/java/com/example/tryggaklassenpod/dataClasses/Episode.kt
@@ -1,13 +1,13 @@
 package com.example.tryggaklassenpod.dataClasses
 
 data class Episode(
-    val id: Int,
-    val imageUrl: String,
-    val episodeUrl: String,
-    val duration: Int,
+    val id: Int = 0,
+    val imageUrl: String = "",
+    val episodeUrl: String = "",
+    val duration: Int = 0,
 
-    val title: String,
-    val description: String,
-    val comments: List<Comments> = emptyList(),
-    val categories: List<String> = emptyList()
+    val title: String = "",
+    val description: String = "",
+    val comments: List<Comments>? = emptyList(),
+    val categories: List<String>? = emptyList()
 )
