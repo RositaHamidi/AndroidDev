9a98c30e70af2363d619545f72a18a0fbd2190fd
Liis U <liis.usin@gmail.com>
2023-10-10 12:01:54 +0200

    add LoadingSpinner and dependency for fetching data

---
+++ app/src/main/java/com/example/tryggaklassenpod/ui/components/LoadingSpinner.kt
@@ -0,0 +1,28 @@
+package com.example.tryggaklassenpod.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun LoadingSpinnerPreview() {
+    LoadingScreen()
+}
