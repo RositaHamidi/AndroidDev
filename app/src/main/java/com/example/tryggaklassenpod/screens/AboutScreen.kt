958e7f390b1368508a6d834218978471d053492e
RositaHamidi <130679452+RositaHamidi@users.noreply.github.com>
2023-10-11 14:27:06 +0200

    please keep minsdk=23

--- app/src/main/java/com/example/tryggaklassenpod/screens/AboutScreen.kt
+++ app/src/main/java/com/example/tryggaklassenpod/screens/AboutScreen.kt
@@ -1,6 +1,5 @@
 package com.example.tryggaklassenpod.screens
 
-
 import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
@@ -14,51 +13,99 @@ import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
 import com.example.tryggaklassenpod.R
+import com.example.tryggaklassenpod.ui.theme.*
 
 
 @Composable
 fun AboutScreen(navController: NavController){
 
     Column (
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.padding(10.dp))
+
+        Text(text = "The work to prevent ill health is shared: " +
+                "the municipality, authorities, companies, associations, " +
+                "students, teachers, parents and organizations are all equally" +
+                " important. Together we can be supportive role models and" +
+                " spread information. \n\nVisit us at: ",
+            modifier = Modifier.fillMaxWidth(.9f),
+            textAlign = TextAlign.Justify,
+            style = LocalTextStyle.current.merge(
+                TextStyle(
+                    lineHeight = 1.5.em,))
+
+        )
+        Spacer(modifier = Modifier.padding(5.dp))
+
         Image(
             painter = painterResource(id = R.drawable.lotus_stiliserad_liten),
             contentDescription = "logo",
             modifier = Modifier.fillMaxWidth(.6f),
         )
 
-        Spacer(modifier = Modifier.padding(10.dp))
-        LinkToLotusModelen()
+        Spacer(modifier = Modifier.padding(5.dp))
+        TextWithLink(txt = "Lotus Modelen", lnk = "https://lotusmodellen.se/wp/")
+
+        Spacer(modifier = Modifier.padding(15.dp))
+        Image(
+            painter = painterResource(id = R.drawable.tryggaklassen_logo1),
+            contentDescription = "logo",
+            modifier = Modifier.fillMaxWidth(.6f),
+        )
+
+        Spacer(modifier = Modifier.padding(5.dp))
+        TextWithLink(txt = "Trggaklassen", lnk = "https://tryggaklassen.se/")
+
+        Spacer(modifier = Modifier.padding(15.dp))
+        Image(
+            painter = painterResource(id = R.drawable.placeholder),
+            contentDescription = "logo",
+            modifier = Modifier.fillMaxWidth(.6f),
+        )
+
+        Spacer(modifier = Modifier.padding(5.dp))
+        TextWithLink(txt = "placeholder", lnk = "https://google.com/")
+
+        Spacer(modifier = Modifier.padding(15.dp))
     }
 }
 
+
 @Composable
-fun LinkToLotusModelen() {
+fun TextWithLink(txt: String, lnk: String){
     val context = LocalContext.current
+
     val annotatedString = buildAnnotatedString {
-        append("Visit our website: ")
+        //append("Visit us at ")
         pushStringAnnotation(
             tag = "LINK",
-            annotation = "https://lotusmodellen.se/wp/"
+            annotation = lnk
         )
         withStyle(
             style = SpanStyle(
-                color = MaterialTheme.colorScheme.primary,
+                color = md_theme_light_primary,
                 fontWeight = FontWeight.Bold,
                 fontSize = 16.sp
             )
         ) {
-            append("Lotus Modelen")
+            append(txt)
         }
         pop()
     }
@@ -79,4 +126,11 @@ fun LinkToLotusModelen() {
             }
         }
     )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AboutPreview(){
+    AboutScreen(rememberNavController())
 }
